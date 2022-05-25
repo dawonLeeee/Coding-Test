d7e07beb1669966d4251afc41400afd318173453
dawon <ekdnjs210@gmail.com>
2022-05-25 15:09:51 +0900

    question11654

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("A");
+        Scanner scanner = new Scanner(System.in);
+
+        char ch = scanner.nextLine().charAt(0);
+
+        int num = (int)ch;
+        System.out.println(num);
     }
 }
