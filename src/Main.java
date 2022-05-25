36de567a5a453a7f66eeaac58b8cd7813bf4b582
dawon <ekdnjs210@gmail.com>
2022-05-25 15:58:15 +0900

    question2675

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,29 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 public class Main {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        String S = in.next();
-        int[] arr = new int[26];
-        for(int i = 0; i<26; i++)
-            arr[i] = -1;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        byte[] str = S.getBytes();
+        for(int i = 0; i<T; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int R = Integer.parseInt(st.nextToken());
+            String S = st.nextToken();
 
-        for(int i = 0; i<S.length(); i++){
-            char ch = S.charAt(i);
-            if(arr[ch-'a'] == -1)
-                arr[ch-'a'] = i;
+            for(int j = 0; j<S.length(); j++){
+                for(int a = 0; a<R; a++)
+                    sb.append(S.charAt(j));
+            }
+            sb.append("\n");
+
         }
-
-        for(int val: arr)
-            System.out.print(val + " ");
+        System.out.println(sb);
 
     }
 }
