bde47866cd49683c4e244b887dfafca292c11699
dawon <ekdnjs210@gmail.com>
2022-05-30 16:48:01 +0900

    question_10870

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,14 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        System.out.println(factorial(N));
 
+
+        System.out.println(fibonacci(N));
     }
-    public static int factorial(int N){
-        if(N<= 1) return 1;
-        return N * factorial(N - 1);
+    static int fibonacci(int N){
+        if(N == 0) return 0;
+        if(N == 1) return 1;
+
+        return fibonacci(N - 1) + fibonacci(N - 2) ;
     }
 }
