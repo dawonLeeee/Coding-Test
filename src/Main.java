bd6d4f6d2dc69e5fe99a40d416e6b4a27488babb
dawon <ekdnjs210@gmail.com>
2022-05-30 13:12:34 +0900

    question_2292

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,18 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-    int A = Integer.parseInt(st.nextToken());
-    int B = Integer.parseInt(st.nextToken());
-    int C = Integer.parseInt(st.nextToken());
-        long count = 1;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int num = 0;  int cnt = 0;
 
-    if(B>=C)
-        System.out.println("-1");
-    else {
 
-        while (true) {
-            if (A >= ((C - B) * count))
-                count++;
-            else break;
-        }
-        System.out.println(count);
-    }
+         for(int i = 0; i<1000000000; i++){
+              cnt += i;
+              num =  1 + cnt*6;
+              if(N <= num) {
+                   System.out.println(i+1);
+                   break;
+              }
+         }
     }
 }
