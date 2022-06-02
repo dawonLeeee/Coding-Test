a63bf2db6cf356cecd2effa5e67af601310e84b4
dawon <ekdnjs210@gmail.com>
2022-06-02 16:08:30 +0900

    question_2798

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,46 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-    static String underbar = "";
-     static void fibonacci(int N){
-        String line = underbar;
-        if(N == 0){
-            System.out.println(line + "\"재귀함수가 뭔가요?\"");
-            System.out.println(line + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
-            System.out.println(line + "라고 답변하였지.");
-            return;
-       } else {
-            System.out.println(line + "\"재귀함수가 뭔가요?\"");
-            System.out.println(line + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
-            System.out.println(line + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-            System.out.println(line + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-            underbar += "____";
-            fibonacci(N - 1);
-            System.out.println(line + "라고 답변하였지.");
 
+        int result = search(arr, N, M);
+        System.out.println(result);
     }
 
-    public static void main(String[] args) throws IOException {
+    static int search(int[] arr, int N, int M) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        int result = 0;
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                for (int k = j+1; k < N; k++) {
+                    int sum = (arr[i] + arr[j] + arr[k]);
 
-        System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
-        fibonacci(N);
+                    if (sum == M)
+                        return sum;
+
+                    if ((sum < M) && (sum > result))
+                        result = sum;
+                }
+            }
+        }
+        return result;
     }
 }
