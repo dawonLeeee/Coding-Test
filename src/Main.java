c0e84d55c4a001a381bfead428acbd9b2e39d709
dawon <ekdnjs210@gmail.com>
2022-06-02 18:12:07 +0900

    question_2231

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
 
+        int i;
+        for (i = 0; i <= N; i++) {
+            int number = i;
+            int sum = 0;
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+            while (number != 0) {
+                sum += number % 10;
+                number /= 10;
+            }
 
-
-        int[] arr = new int[N];
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        int result = search(arr, N, M);
-        System.out.println(result);
-    }
-
-    static int search(int[] arr, int N, int M) {
-
-        int result = 0;
-        for (int i = 0; i < N - 2; i++) {
-            for (int j = i + 1; j < N - 1; j++) {
-                for (int k = j+1; k < N; k++) {
-                    int sum = (arr[i] + arr[j] + arr[k]);
-
-                    if (sum == M)
-                        return sum;
-
-                    if ((sum < M) && (sum > result))
-                        result = sum;
-                }
+            if (sum + i == N) {
+                break;
+            }
+            if (i == N) {
+                i = 0;
+                break;
             }
         }
-        return result;
+        System.out.println(i);
     }
 }
