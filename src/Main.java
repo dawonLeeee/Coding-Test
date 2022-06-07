a6df87a1953dfcd04461fa3779da68c2a8d4ed56
dawon <ekdnjs210@gmail.com>
2022-06-07 15:32:48 +0900

    question_1436

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,20 @@
 import java.io.IOException;
-import java.util.StringTokenizer;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
 public class Main {
-
-    public static boolean[][] arr;
-    public static int min = 64;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        int count= 1;
+        int num = 666;
 
-        arr = new boolean[N][M];
-
-        for (int i = 0; i < N; i++) {
-            String str = br.readLine();
-
-            for (int j = 0; j < M; j++) {
-                if (str.charAt(j) == 'W') {
-                    arr[i][j] = true;
-                } else {
-                    arr[i][j] = false;
-                }
+        while(count != N){
+            num++;
+            if(String.valueOf(num).contains("666")){
+                count++;
             }
         }
-
-        int N_row = N - 7;
-        int M_col = M - 7;
-
-        for (int i = 0; i < N_row; i++) {
-            for (int j = 0; j < M_col; j++) {
-                find(i, j);
-            }
+        System.out.println(num);
         }
-        System.out.println(min);
-
-    }
-        public static void find(int x, int y){
-            int end_x = x + 8;
-            int end_y = y + 8;
-            int count = 0;
-
-            boolean TF = arr[x][y];
-
-            for(int i = x; i < end_x; i++){
-                for(int j = y; j < end_y; j++){
-
-                    if(arr[j][j] != TF){
-                        count++;
-                    }
-                    TF = (!TF);
-                }
-                TF  = !TF;
-            }
-
-
-            count = Math.min(count, 64-count);
-
-
-            min = Math.min(min, count);
-        }
-
 }
