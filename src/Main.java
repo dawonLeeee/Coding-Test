21e8a7e47cbc3e3364452017ae10675b0b5830fc
dawon <ekdnjs210@gmail.com>
2022-06-09 15:23:45 +0900

    question_1181

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.*;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        int[][] arr = new int[N][2];
+        String[] arr = new String[N];
 
-        for(int i = 0; i < N; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
         }
+        HashSet<String> set = new HashSet<>(List.of(arr));
+        String[] brr = set.toArray(new String[0]);
 
-        Arrays.sort(arr, new Comparator<int[]>() {
+        Arrays.sort(brr, new Comparator<String>() {
             @Override
-            public int compare(int[] o1, int[] o2) {
-                if(o1[1] == o2[1])
-                    return o1[0] - o2[0];
+            public int compare(String o1, String o2) {
+                if(o1.length() == o2.length())
+                    return o1.compareTo(o2);
                 else
-                    return o1[1] - o2[1];
+                    return o1.length() - o2.length();
+
+
             }
+
         });
 
-        for(int i = 0; i< N; i++){
-            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+        for(int i = 0; i < brr.length; i++){
+            sb.append(brr[i]).append("\n");
         }
         System.out.println(sb);
-
     }
 }
