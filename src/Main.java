50a1ea800501891f8d9c4e414c53d46d48ba020b
dawon <ekdnjs210@gmail.com>
2022-06-09 14:40:28 +0900

    question_11651

--- src/Main.java
+++ src/Main.java
@@ -8,36 +8,33 @@ import java.util.StringTokenizer;
 
 
 public class Main {
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-
-        int N = Integer.parseInt(br.readLine());
         int[][] arr = new int[N][2];
 
         for(int i = 0; i < N; i++){
-            st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine(), " ");
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr, new Comparator<int[]>(){
+        Arrays.sort(arr, new Comparator<int[]>() {
             @Override
-            public int compare(int[] e1, int[] e2){
-                if(e1[0] == e2[0])
-                    return e1[1] - e2[1];
+            public int compare(int[] o1, int[] o2) {
+                if(o1[1] == o2[1])
+                    return o1[0] - o2[0];
                 else
-                    return e1[0] - e2[0];
+                    return o1[1] - o2[1];
             }
         });
 
-        for (int[] ints : arr) {
-            sb.append(ints[0]).append(" ").append(ints[1]).append("\n");
+        for(int i = 0; i< N; i++){
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
         }
         System.out.println(sb);
 
-        }
+    }
 }
