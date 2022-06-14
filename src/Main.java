c12942989d6b5dfb6c41e683fb05025284100cf2
dawon <ekdnjs210@gmail.com>
2022-06-14 14:39:12 +0900

    question_10816

--- src/Main.java
+++ src/Main.java
@@ -5,31 +5,31 @@ import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] arr;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+    static int N;
+    static HashMap<Integer, Integer> map = new HashMap<>();
+    static StringTokenizer st;
+    static BufferedReader br;
 
-        HashMap<String, String> map = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
 
 
         for(int i = 0; i < N; i++){
-            String pocketmon = br.readLine();
-            String number = Integer.toString(i+1);
-            map.put(pocketmon, number);
-            map.put(number, pocketmon);
+            int key = Integer.parseInt(st.nextToken());
+            map.put(key, map.getOrDefault(key, 0)+1);
         }
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
 
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < M; i++){
-            String key = br.readLine();
-            sb.append(map.get(key)).append("\n");
+            int number = Integer.parseInt(st.nextToken());
+            sb.append(map.getOrDefault(number, 0)).append(" ");
         }
         System.out.println(sb);
+
     }
-
-
-
 }
