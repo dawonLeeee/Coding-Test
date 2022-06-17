b5995800b6becdc5d8daec4ebdec92bf22f76b0a
dawon <ekdnjs210@gmail.com>
2022-06-17 09:20:49 +0900

    question_1085

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.StringTokenizer;
+
 
 public class Main {
        public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
 
-        HashSet<String> set = new HashSet<>();
+        int min = Math.min(Math.abs(x-w), Math.abs(y-h));
+        if(min > x)
+            min = x;
+        if(min > y)
+            min = y;
+        System.out.println(min);
 
 
-        for(int i = 0; i <= s.length(); i++){
-            for(int j = i+1; j <= s.length(); j++){
-                set.add(s.substring(i,j));
-            }
-        }
-        System.out.println(set.size());
         }
 }
