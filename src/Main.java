5a9c45604d0c426cc67b570c140dda2151f5d0f3
dawonLeeee <ekdnjs210@gmail.com>
2022-08-08 12:25:03 +0900

    question 1987 소수 찾기

--- src/Main.java
+++ src/Main.java
@@ -3,42 +3,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Main {
-    
-    static int gcd(int a, int b) { // Greatest Common Number
-    	swap(a, b);
-    	
-    	while(b != 0) {
-    		int r = a % b;
-	    	a = b;
-	    	b = r;
-    	}
-    	return a;
-    }
-    
-    static void swap(int a, int b) {
-    	if(a < b) {
-    		int tmp;
-    		tmp = a;
-    		a = b;
-    		b = tmp;
-    	}
-    }
-    
-    
-    
-
-    public static void main(String[] args) throws IOException {
-       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-       StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-       int n1 = Integer.parseInt(st.nextToken());
-       int n2 = Integer.parseInt(st.nextToken());
-           
-       int gcd = gcd(n1, n2);
-       int lcd = n1 * n2 / gcd;
-       System.out.println(gcd);
-       System.out.println(lcd);
+	
+	static int count = 0;
+	
+     public static void main(String[] args) throws IOException  {
+    	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	   br.readLine();
+    	   
+    	   
+    	   StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    	   while(st.hasMoreElements()) {
+    		   int n =Integer.parseInt(st.nextToken());
+    		   if(isPrime(n) == true)
+    			   count++;
+    	   }
+    	   System.out.println(count);   
        }
        
+     
+     
+       public static boolean isPrime(int n) {
+    	   if(n <= 1)
+    		   return false;
+    	   
+    	   for(int i = 2; i <= Math.sqrt(n); i++) {
+    		   if(n % i == 0) return false;
+    	   }
+    	   return true;    	   
+       }
+       
+       //시간복잡도가 O(n log(log n))인 방법
+       public boolean[] make_prime(int Max) {
+    	   boolean[] prime = new boolean[Max + 1]; // 0부터 시작하므로 +1
+    	   prime[0] = true;
+    	   prime[1] = true;
+    	   
+    	  
+    	   for(int i = 2; i <= Math.sqrt(Max); i++) {
+    		   //이미 걸러진 배열일 경우 다음 반복문으로 건너뜀
+    		   if(prime[i] == true)
+    			   continue;
+    		   
+    		   for(int j = i; j < Max + 1; j = j + i) {
+    			   prime[j] = true;
+    		   }
+    	   }
+    	   return prime;    	   
+       }
 }
