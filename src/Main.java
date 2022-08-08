7abc4d45e6c73b9aeb0dddba1a5db74f6b4d72d5
dawonLeeee <ekdnjs210@gmail.com>
2022-08-08 16:39:09 +0900

    question 2351 소수

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
+	
+	public static boolean[] prime;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
 		
-		int count = 0;
+		int M = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		
+		prime = new boolean[N+1];
+		get_prime();
+		
 		int sum = 0;
-		for(int i = 1; i < 50; i++) {
-			for(int j = 1; j <= i; j++) {
-				count++;
-				if(count >= A && count <= B) {
-					
-					
-					sum += i;
-				}
+		int min = Integer.MAX_VALUE;
+		for(int i = M; i <= N; i++) {
+			if(!prime[i]) {
+				sum += i;
+				if(min == Integer.MAX_VALUE)
+					min = i;
 			}
-			
 		}
-		System.out.println(sum);
+		if(sum == 0)
+			System.out.println(-1);
+		else {
+			System.out.println(sum);
+			System.out.println(min);
+		}
+			
+		
 	}
+
+		
+		public static void get_prime() {
+			prime[0] = true;
+			prime[1] = true;
+			
+			for(int i = 2; i <= Math.sqrt(prime.length); i++) {
+				if(prime[i]) {continue;}
+				for(int j = i * i; j < prime.length; j += i)
+					prime[j] = true;
+			}
+		}
+	
 }
