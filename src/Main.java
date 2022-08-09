86166c1a03d9b16f0b8d6c6edb2ed597451c4aeb
nanazzoo <ekdnjs210@gmail.com>
2022-08-09 18:18:00 +0900

    question 1541 잃어버린 괄호
    그리디 배열

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
-
-
-class Time implements Comparable<Time> {
-	int start;
-	int end;
-	Time(int start, int end) {
-		this.start = start;
-		this.end = end;
-	}
-	
-	@Override
-	public int compareTo(Time t) {
-		if(this.end == t.end)
-			return this.start - t.start;
-		return this.end - t.end;
-	}
-}
 
 
 public class Main  {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		ArrayList<Time> timeList = new ArrayList<>();
-		StringTokenizer st;
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			timeList.add(new Time(a, b));
-		}
+		StringTokenizer st_minus = new StringTokenizer(br.readLine(), "-");
 		
-		Collections.sort(timeList);
+		int sum = 0;
+		StringTokenizer st_num;
 		
-		int ans = 1;
-		int endTime = timeList.get(0).end;
-		for(int i = 1; i < timeList.size(); i++) {
-			if(timeList.get(i).start >= endTime) {
-				endTime = timeList.get(i).end;
-				ans++;
+		if(st_minus.countTokens() >= 1) {
+			st_num = new StringTokenizer(st_minus.nextToken(), "-|+");
+			while(st_num.hasMoreTokens())
+				sum += Integer.parseInt(st_num.nextToken());
+			while(st_minus.hasMoreTokens()) {
+				st_num = new StringTokenizer(st_minus.nextToken(), "-|+");
+				while(st_num.hasMoreTokens())
+					sum -= Integer.parseInt(st_num.nextToken());
 			}
+		} else {
+			st_num = new StringTokenizer(st_minus.nextToken(), "+");
+			while(st_num.hasMoreTokens())
+				sum += Integer.parseInt(st_num.nextToken());
 		}
-		System.out.println(ans);
+		
+		System.out.println(sum);
+		
 	}
 }
 	
