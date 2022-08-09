afc3b3e0498c9178c322e8230d6f49c349d6f4f4
nanazzoo <101784680+nanazzoo@users.noreply.github.com>
2022-08-09 11:25:07 +0900

    question 2839 설탕배달

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,23 @@ public class Main  {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		long num = Long.valueOf(br.readLine());
-		long sum = 0;
-		long count = 0;
+		int N = Integer.parseInt(br.readLine());
+		int result = 0;
 		
-		for(int i = 1; ; i++) {
-			if(sum > num) break;
-			sum += i;
-			count++;
+		
+		if(N == 4 || N == 7) {
+			result = -1;
+		} else 
+			switch(N % 5) {
+				case 1 : result = (N / 5) - 1 + 2; break;
+				case 2 : result = (N / 5) - 2 + 4; break;
+				case 3 : result = (N / 5) + 1; break;
+				case 4 : result = (N / 5) - 1 + 3; break;
+				default : result = (N / 5);
+			
 		}
-		System.out.println(count - 1);
-		
+		System.out.println(result);
 	}
-	
-	
 }
 
 
