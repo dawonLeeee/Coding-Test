5da428780d9d674cc171eb3912d95b843b866103
nanazzoo <ekdnjs210@gmail.com>
2022-08-09 16:42:24 +0900

    question 1931 회의실 배정
    그리디 알고리즘

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
-public class Main  {
+
+class Time implements Comparable<Time> {
+	int start;
+	int end;
+	Time(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
 	
+	@Override
+	public int compareTo(Time t) {
+		if(this.end == t.end)
+			return this.start - t.start;
+		return this.end - t.end;
+	}
+}
+
+
+public class Main  {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int[] arr = new int[N];
-		int sum = 0;
+		ArrayList<Time> timeList = new ArrayList<>();
+		StringTokenizer st;
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			timeList.add(new Time(a, b));
+		}
 		
-		int i = 0;
-		while(st.hasMoreTokens())
-			arr[i++] = Integer.parseInt(st.nextToken());
+		Collections.sort(timeList);
 		
-		Arrays.sort(arr);
-		
-		for(i = 0; i < N; i++) 
-			sum += arr[i] * (N - i);
-			
-		System.out.println(sum);
+		int ans = 1;
+		int endTime = timeList.get(0).end;
+		for(int i = 1; i < timeList.size(); i++) {
+			if(timeList.get(i).start >= endTime) {
+				endTime = timeList.get(i).end;
+				ans++;
+			}
+		}
+		System.out.println(ans);
 	}
-	
 }
+	
+	
+
 
 
