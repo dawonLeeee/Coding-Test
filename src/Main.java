f4cf945e3452572a1f8ec56134a0d57b146f7fb9
dawonLeeee <102362636+dawonLeeee@users.noreply.github.com>
2022-08-11 12:45:28 +0900

    question 10828 스택
    자료구현 문제

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,91 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 
 
 public class Main  {
-	
-	
+	public static Integer[] arr;
+	public static int size = 0;
 	
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
 		int T = Integer.parseInt(br.readLine());
 		
-		StringBuilder sb = new StringBuilder();
+		arr = new Integer[T];
 		
+		
+		
+
+		int i = 0;
 		while(T-- > 0) {
-			sb.append(solve(br.readLine())).append("\n");
+			st = new StringTokenizer(br.readLine(), " ");
+			
+
+			switch(st.nextToken()) {
+				case("push") : 
+					push(Integer.parseInt(st.nextToken())); break;
+				case("pop") :	sb.append(pop()).append("\n"); break;
+				case("size") : sb.append(size()).append("\n"); break;
+				case("empty") : sb.append(empty()).append("\n"); break;
+				case("top") : sb.append(top()).append("\n"); break;
+			}
 		}
 		System.out.println(sb);
 		
 		
 	}
 	
-	public static String solve(String s) {
-		int count = 0;
-		for(char c: s.toCharArray()) {
-			if(c == '(') 
-				count++;
-			else if(count == 0)
-				return "NO";
-			else
-				count--;
-		}
-		if(count == 0)
-			return "YES";
-		else
-			return "NO";
+	
+	
+	
+	public static void push(int num) {
+		 arr[size++] = num;	 
 	}
+	
+	static int pop() {
+		if(size == 0)
+			return -1;
+		else {
+			int pop = arr[--size];
+			arr[size] = null;
+			return pop;
+			
+		}
+	}
+	
+	static int size() {
+		return size;
+	}
+	
+	static int empty() {
+		if(size == 0)
+			return 1;
+		else
+			return 0;
+	}
+
+	static int top() {
+		if(size == 0)
+			return -1;
+		else
+			return arr[size - 1];
+	}
+	
+	
+		
 		
 }
+
+
+
+	
+
 	
 	
 
