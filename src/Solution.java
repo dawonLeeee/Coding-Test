0645d803e866f27b3073cedfb0f6f77e95504cca
dawonLeeee <102362636+dawonLeeee@users.noreply.github.com>
2022-08-12 14:29:29 +0900

    프로그래머스 없는 숫자 더하기
    매우 쉬움

--- src/Solution.java
+++ src/Solution.java
@@ -1,33 +1,24 @@
-import java.util.Stack;
 
-public class Solution  {
-	static int answer;
-	static Stack<Integer> stack;
-	
-	
-	
-	 public int solution(int[][] board, int[] moves) {
-	        answer = 0;
-	        
-	        boolean flag = false;
-	        stack = new Stack<>();
-	        stack.push(0);
-	        for(int i: moves) {
-	        	for(int j = 0; j < board.length; j++) {
-	        		while(board[j][i-1] != 0) {
-	        			if(stack.peek() == board[j][i-1]) {
-	        				stack.pop();
-	        				answer += 2;
-	        				
-	        			} else {
-	        				stack.push(board[j][i-1]);
-	        			}
-	        			board[j][i-1] = 0;
-	        			break;
-	        		}
-	        	}
-	        }
-	        return answer;
-	    }
+
+
+public class Solution {
+	public int solution(int[] numbers) {
+        int answer = 0;
+        int sumOneToTen = 0;
+        int sum = 0;
+        
+        
+        
+        for(int n: numbers)
+        	sum += n;
+        
+        
+        
+        for(int i = 0; i < 10; i++)
+        	sumOneToTen += i;
+        
+        
+        answer = sumOneToTen - sum;
+        return answer;
+    }
 }
-
