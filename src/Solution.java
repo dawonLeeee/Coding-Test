a54a6550fd2b0c015f3a3b342c37852256f794f3
dawonLeeee <102362636+dawonLeeee@users.noreply.github.com>
2022-08-16 09:08:16 +0900

    프로그래머스 내적

--- src/Solution.java
+++ src/Solution.java
@@ -1,19 +1,11 @@
-
-
-
-public class Solution {
-	public int solution(int[] absolutes, boolean[] signs) {
-		int answer = 0;
-		for(int i = 0; i < signs.length; i++) {
-			answer += returnAbs(absolutes[i], signs[i]);
-		}
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+    	
+    	for(int i = 0; i < a.length; i++) {
+        	answer += a[i] * b[i];
+        }
 
         return answer;
     }
-	
-	public int returnAbs(int num, boolean flag) {
-		if(!flag)
-			num = -num;
-		return num;
-	}
 }
