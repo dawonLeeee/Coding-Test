8f120b1246916363d0c6c4066c04b4677804ee8a
dawonLeeee <102362636+dawonLeeee@users.noreply.github.com>
2022-08-23 17:10:39 +0900

    프로그래머스 (나누어 떨어지는 숫자 배열)

--- src/Solution.java
+++ src/Solution.java
@@ -1,62 +1,31 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 class Solution {
-    public static int[] solution(int[] answers) {
-        int[] answer;
+    public int[] solution(int[] arr, int divisor) {
+        int[] answer = {};
+        int count = 0;
+        ArrayList<Integer> list = new ArrayList<>();
         
-        int[] arrA = {1, 2, 3, 4, 5};
-        int[] arrB = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] arrC = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int a, b, c;
-        int countA = 0; 
-        int countB = 0;
-        int countC = 0;
-        int max = Integer.MIN_VALUE;
-        
-        for(int i = 0; i < answers.length; i++) {
-            
-        	
-        	if(i == 0) { a = b = c = 0;}
-        	else {
-	        	a = i % arrA.length;
-	        	b = i % arrB.length;
-	        	c = i % arrC.length;
+        for(int i : arr) {
+        	if(i % divisor == 0) {
+        		count++;
+        		list.add(i);
         	}
-        	
-        	if(arrA[a] == answers[i]) countA++;
-        	if(arrB[b] == answers[i]) countB++;
-        	if(arrC[c] == answers[i]) countC++;
+        }
+
+        
+        if(count == 0) answer = new int[] {-1};
+        else {
+        	answer = new int[count];
+        	int cnt = 0;
+        	for(int i : list)
+        		answer[cnt++] = i;
+        		
         }
         
-        System.out.println("countA : " + countA);
-        System.out.println("countB : " + countB);
-        System.out.println("countC : " + countC);
+        Arrays.sort(answer);
         
-        
-        max = Math.max(Math.max((Math.max(max, countA)), countB), countC);
-        
-        if(max == countA && max == countB && max == countC) {
-        	answer = new int[]{1,2, 3};
-    	}else if(max == countA && max == countB) {
-        	answer = new int[]{1,2};
-        } else if(max == countA && max == countC) {
-        	answer = new int[]{1,3};
-        } else if(max == countB && max == countC) {
-        	answer = new int[] {2, 3};
-        } else
-        	answer = new int[] {
-        			(max == countA ? 1 : (max == countB ? 2 : 3))
-        	};
-        
- 
         return answer;
     }
-    
-    public static void main(String[] args) {
-		int[] answers1 = {1, 2, 3, 4, 5};
-		int[] answers2 = {1,3,2,4,2};
-		
-		Solution solution = new Solution();
-		System.out.println(Arrays.toString(solution(answers2)));
-	}
 }
