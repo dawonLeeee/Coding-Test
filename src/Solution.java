25d74a6bbbc5f814f383f6855d0190ae68ef35b6
dawonLeeee <102362636+dawonLeeee@users.noreply.github.com>
2022-08-24 12:03:38 +0900

    프로그래머스 (체육복)

--- src/Solution.java
+++ src/Solution.java
@@ -1,31 +1,53 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+
 
 class Solution {
-    public int[] solution(int[] arr, int divisor) {
-        int[] answer = {};
-        int count = 0;
-        ArrayList<Integer> list = new ArrayList<>();
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        int[] arr = new int[n];
         
-        for(int i : arr) {
-        	if(i % divisor == 0) {
-        		count++;
-        		list.add(i);
+        for(int i : lost)
+        	arr[i-1]--;
+        for(int i : reserve)
+        	arr[i-1]++;
+        
+        if(arr[0] == -1) {
+        	if(arr[1] == 1) {
+        		arr[0] = 0;
+        		arr[1] = 0;
         	}
         }
-
-        
-        if(count == 0) answer = new int[] {-1};
-        else {
-        	answer = new int[count];
-        	int cnt = 0;
-        	for(int i : list)
-        		answer[cnt++] = i;
-        		
+  
+        for(int i = 1; i < n - 1; i++) {
+        	if(arr[i] == -1) {
+        		if(arr[i-1] == 1) {
+        			arr[i-1] = 0;
+        			arr[i] = 0;
+        		} else if(arr[i+1] == 1) {
+        			arr[i] = 0;
+        			arr[i+1] = 0;
+        		}
+        	}
+        }
+        if(arr[n-1] == -1) {
+        	if(arr[n-2] == 1) {
+        		arr[n-1] = 0;
+        		arr[n-2] = 0;
+        	}
         }
         
-        Arrays.sort(answer);
+        for(int i: arr)
+        	if(i != -1) answer++;
+        
+        	
+        
         
         return answer;
     }
+    
+    public static void main(String[] args) {
+		Solution sol = new Solution();
+		int[] lost = {2, 4};
+		int[] reserve = {1, 3, 5};
+		System.out.println(sol.solution(5, lost, reserve));
+	}
 }
