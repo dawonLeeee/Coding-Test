8c439b50d1f934610d9082843c1abbbdd2275cfb
dawonLeeee <102362636+dawonLeeee@users.noreply.github.com>
2022-09-02 09:00:08 +0900

    프로그래머스 (다트 게임)

--- src/Solution.java
+++ src/Solution.java
@@ -1,40 +1,83 @@
-import java.util.ArrayList;
+
+import java.util.Stack;
+
 
 class Solution {
-	static ArrayList<Integer> list = new ArrayList<>();
-	
-    public int solution(int n) {
-        String str = "";
+    public int solution(String dartResult) {
+        int answer = 0;
+        int count = 0;
+        int num;
         
-        tripleNum(n);
-    	for(int i : list) {
-    		str += i;
-    	}
-    	System.out.println("str : " + str);
-    	int num = Integer.parseInt(str);
-    	System.out.println("num : " + num);
-    	
+        Stack<Character> stack = new Stack<>();
+        int[] arr = new int[3];
 
-    	int answer = Integer.parseInt(str, 3);
-  
-    	
-    	return answer;
-    }
 
-    public void tripleNum(int n) {
-    	if(n == 0) return;
-    		
-    	list.add(n%3);
-    	tripleNum(n= n/3);
+
+        for(int i = 0; i < dartResult.length() -1;) {
+        	int cal = 1;
+
+        	while(dartResult.charAt(i) <= '9' && dartResult.charAt(i) >= '0') 
+        		stack.add(dartResult.charAt(i++));
+        	
+        	
+        	stack.add(dartResult.charAt(i++)); // i = 2
+
+        	if(i< dartResult.length()) {
+	        	if((Character.compare(dartResult.charAt(i), '*') == 0)) {
+	        		if(count != 0) arr[count-1] *= 2;
+	        		System.out.println("2배 적용한 arr : " + arr[count]); // @@@@@@@@@@@@@@@@@@@@@@@2
+	        		cal = 2;
+	        		i++;
+	        	} else if(dartResult.charAt(i) - '#' == 0) {
+	        		cal  = -1;
+	        		i++;
+	        	} 
+        	}
+//        	System.out.println("##cal : " + cal);
+        	
+
+        	char tmp = stack.pop();
+        	
+        	if(stack.size() >= 2) {
+        		num = 10;
+        		stack.clear();
+        	}
+        	else
+        		num = stack.pop() - 48;
+        	System.out.println("num : " + num);// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        		
+
+	        switch(tmp) {
+		       	case 'S' : 
+		       		cal *= num;
+
+		       		break;
+		       	case 'D' : 
+		       		cal *= num * num;
+
+		       		break;
+		       	case 'T' : 
+		       		cal *= num * num * num;
+
+		       		break;
+	        }
+	        System.out.println("cal : " + cal);// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@22
+
+	        
+	        arr[count++] += cal;
+	        System.out.println("arr[count] : " + arr[count-1]);// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@2
+        }
+
+        for(int i : arr)
+        	answer += i;
+        return answer;
     }
-    
     
     public static void main(String[] args) {
 		Solution sol = new Solution();
 		
 		
-		int n = 45;
-		
-		System.out.println("답 : " + sol.solution(n));
+		String str1 = "10D10S0D";
+		System.out.println(sol.solution(str1));
 	}
 }
