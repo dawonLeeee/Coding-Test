c37ab538b5195691d03579ec63381b6c7c89d811
dawonLeeee <102362636+dawonLeeee@users.noreply.github.com>
2022-09-02 14:00:40 +0900

    프로그래머스 (부족한 금액 계산하기)

--- src/Solution.java
+++ src/Solution.java
@@ -1,83 +1,25 @@
-
-import java.util.Stack;
-
-
 class Solution {
-    public int solution(String dartResult) {
-        int answer = 0;
-        int count = 0;
-        int num;
+    public long solution(int price, int money, int count) {
+        int sum = 0;
+
         
-        Stack<Character> stack = new Stack<>();
-        int[] arr = new int[3];
+        
+        long answer = (long)price * (count * (count+1) / 2) - money;
+        if(answer <= 0) answer = 0;
+        
 
-
-
-        for(int i = 0; i < dartResult.length() -1;) {
-        	int cal = 1;
-
-        	while(dartResult.charAt(i) <= '9' && dartResult.charAt(i) >= '0') 
-        		stack.add(dartResult.charAt(i++));
-        	
-        	
-        	stack.add(dartResult.charAt(i++)); // i = 2
-
-        	if(i< dartResult.length()) {
-	        	if((Character.compare(dartResult.charAt(i), '*') == 0)) {
-	        		if(count != 0) arr[count-1] *= 2;
-	        		System.out.println("2배 적용한 arr : " + arr[count]); // @@@@@@@@@@@@@@@@@@@@@@@2
-	        		cal = 2;
-	        		i++;
-	        	} else if(dartResult.charAt(i) - '#' == 0) {
-	        		cal  = -1;
-	        		i++;
-	        	} 
-        	}
-//        	System.out.println("##cal : " + cal);
-        	
-
-        	char tmp = stack.pop();
-        	
-        	if(stack.size() >= 2) {
-        		num = 10;
-        		stack.clear();
-        	}
-        	else
-        		num = stack.pop() - 48;
-        	System.out.println("num : " + num);// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-        		
-
-	        switch(tmp) {
-		       	case 'S' : 
-		       		cal *= num;
-
-		       		break;
-		       	case 'D' : 
-		       		cal *= num * num;
-
-		       		break;
-		       	case 'T' : 
-		       		cal *= num * num * num;
-
-		       		break;
-	        }
-	        System.out.println("cal : " + cal);// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@22
-
-	        
-	        arr[count++] += cal;
-	        System.out.println("arr[count] : " + arr[count-1]);// @@@@@@@@@@@@@@@@@@@@@@@@@@@@@2
-        }
-
-        for(int i : arr)
-        	answer += i;
+        
         return answer;
     }
     
     public static void main(String[] args) {
 		Solution sol = new Solution();
 		
+		int price = 3;
+		int money = 20;
+		int count = 4;
 		
-		String str1 = "10D10S0D";
-		System.out.println(sol.solution(str1));
-	}
+		
+		System.out.println(sol.solution(price, money, count));
+    }
 }
