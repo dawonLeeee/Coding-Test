164e830601d11de1b538b1635a43db9a372a3588
dawon <ekdnjs210@gmail.com>
2023-08-15 14:22:10 +0900

    baekjoon 배열 - 수열정렬(1015)

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,52 @@
+import javax.sound.sampled.EnumControl;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main{
-	public static void main(String[] args) throws IOException{
+public class Main {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		List<Integer> arr = new ArrayList<>();
-		int maxNum = -1;
-		for(int i = 0; i < 9; i++){
-			st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < 9; j++){
-				int n = Integer.parseInt(st.nextToken());
-				if(maxNum < n) maxNum = n;
-				arr.add(n);
-			}
-		}
-		System.out.println(maxNum);
-		System.out.println((arr.indexOf(maxNum)/9 +1) + " " + (arr.indexOf(maxNum)%9+1));
-	}
-}
+		StringBuilder stringBuilder = new StringBuilder();
+		int N = Integer.parseInt(br.readLine()); // 배열의 크기
 
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int[][] arr = new int[N][2];
+		int[] arr2 = new int[N];
+
+		for(int i = 0; i < N; i++){
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = i;
+		}
+
+		sort(arr);
+
+		for(int i = 0; i < N; i++){
+			arr2[arr[i][1]] = i;
+		}
+
+		for(int i = 0; i < N; i++){
+			System.out.print(arr2[i] + " ");
+		}
+
+
+
+	}
+
+	public static void sort(int[][] solArr){
+
+		Arrays.sort(solArr, (i1,i2) -> {
+			if(i2[0] == i1[0]) {
+				return i1[1] - i2[1];
+			}
+			else return i1[0] - i2[0];
+		});
+
+	}
+
+
+
+
+
+}
 
