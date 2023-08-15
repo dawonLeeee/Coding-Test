555abbd1073a2aa332d0df30e713daab6cd1e56d
dawon <ekdnjs210@gmail.com>
2023-08-15 15:58:18 +0900

    baekjoon 그리다알고리즘 -수열정렬(11047)

--- src/Main.java
+++ src/Main.java
@@ -2,51 +2,40 @@ import javax.sound.sampled.EnumControl;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.sql.Array;
 import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder stringBuilder = new StringBuilder();
-		int N = Integer.parseInt(br.readLine()); // 배열의 크기
-
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int[][] arr = new int[N][2];
-		int[] arr2 = new int[N];
+		StringBuilder stringBuilder = new StringBuilder();
 
-		for(int i = 0; i < N; i++){
-			arr[i][0] = Integer.parseInt(st.nextToken());
-			arr[i][1] = i;
+		int N = Integer.parseInt(st.nextToken()); // 배열의 크기
+		int M = Integer.parseInt(st.nextToken());
+		List<Integer> lst = new ArrayList<>();
+
+		while(true){
+			int a = Integer.parseInt(br.readLine());
+			if(a > M) break;
+			lst.add(a);
 		}
 
-		sort(arr);
-
-		for(int i = 0; i < N; i++){
-			arr2[arr[i][1]] = i;
-		}
-
-		for(int i = 0; i < N; i++){
-			System.out.print(arr2[i] + " ");
-		}
-
-
+		System.out.println(minCnt(lst, M));
 
 	}
 
-	public static void sort(int[][] solArr){
+	public static int minCnt(List<Integer> lst, int M) {
+		int cnt = 0;
+		int total = 0;
+		int i = lst.size() - 1;
+		while(i >= 0 && M != total) {
+			while(M < (total + lst.get(i))) i--;
+			total += lst.get(i);
+			cnt++;
+		}
 
-		Arrays.sort(solArr, (i1,i2) -> {
-			if(i2[0] == i1[0]) {
-				return i1[1] - i2[1];
-			}
-			else return i1[0] - i2[0];
-		});
-
+		return cnt;
 	}
-
-
-
-
-
 }
 
