6ca4205d7357d7cab5bdf6c7726919928ae0d6f0
dawon <ekdnjs210@gmail.com>
2024-03-01 17:06:06 +0900

    프로그래머스 lev1(성격 유형 검사하기)

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,41 @@
 import javax.sound.sampled.EnumControl;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.sql.Array;
 import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		String[] name = {"may", "kein", "kain", "radi"};
-		int[] yearning = {5, 10, 1, 3};
-		String[][] photo = {{"may", "kein", "kain", "radi"},{"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
+		String[] survey = {"TR", "RT", "TR"};
+		int[] choices = {7,1,3};
+		System.out.println(solution(survey, choices));
 
-		solution(name,yearning,photo);
 
 
 
 	}
 
-	public static int[] solution(String[] name, int[] yearning, String[][] photo) {
-		Map<String, Integer> memberScore = new HashMap<>();
-		for(int i = 0; i < name.length; i++){
-			memberScore.put(name[i], yearning[i]);
+	public static String solution(String[] survey, int[] choices) {
+		String answer = "";
+		int rt = 0;
+		int cf = 0;
+		int jm = 0;
+		int an = 0;
+		for(int i = 0; i < survey.length; i++) {
+			int type = 0;
+			if(survey[i].contains("R")) {
+				rt += (survey[i].indexOf("R")==0? 1:-1) * (choices[i]-4);
+			}
+			else if(survey[i].contains("C")) {
+				cf += (survey[i].indexOf("C")==0? 1:-1) * (choices[i]-4);
+			}
+			else if(survey[i].contains("J")) {
+				jm += (survey[i].indexOf("J")==0? 1:-1) * (choices[i]-4);
+			}
+			else if(survey[i].contains("A")) {
+				an += (survey[i].indexOf("A")==0? 1:-1) * (choices[i]-4);
+			}
 		}
 
-		int[] answer = new int[photo.length];
-		for(int i = 0; i < photo.length; i++){
-			int sum = 0;
-			for(int j = 0; j < photo[i].length; j++){
-				sum += memberScore.get(photo[i][j]) == null ? 0 : memberScore.get(photo[i][j]);
-			}
-			answer[i] = sum;
-		}
-		return answer;
+		return (rt>0?"T":"R") + (cf>0?"F":"C") + (jm>0?"M":"J") + (an>0?"N":"A");
 	}
 }
